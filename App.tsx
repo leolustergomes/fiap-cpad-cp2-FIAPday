82092c2b848de4139464cf23dfc24e7e364b4b97
Leonardo Luster Gomes <leolustergomes@gmail.com>
2026-05-04 00:53:44 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Login from './src/pages/login';
+import Cadastro from './src/pages/cadastro';
+import Calendario from './CalendarioNovo';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Login />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+        <Stack.Screen name="Calendario" component={Calendario} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
